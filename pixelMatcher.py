53d5803154db9a3fdd684f647d9e6b32fb95aa10
Julian Jocque <lamabuny@gmail.com>
2017-10-09 22:35:22 -0400

    Get pixel from all images instead of just last child picture

--- pixelMatcher.py
+++ pixelMatcher.py
@@ -2,6 +2,7 @@ from PIL import Image
 from os import listdir
 from os.path import isfile, join
 import numpy as np
+import sys
 
 childFolder = "./images"
 childFiles = [f for f in listdir(childFolder) if isfile(join(childFolder, f))]
@@ -9,45 +10,29 @@ im = Image.open("./BoYT0qQ8.jpg")
 w = im.size[0]
 h = im.size[1]
 
-resultantImageFlat = []
+finalImage = np.zeros((w, h, 3), dtype=np.uint8)
 
-distanceThreshold = 500
+distanceThreshold = int(sys.argv[1]) if len(sys.argv) > 1 else 75
 close = 0
 total = 0
 for file in childFiles:
 	childImage = Image.open(join(childFolder, file))
 	childIterator = iter(childImage.getdata())
-	for pixel in iter(im.getdata()):
-		pixel = np.array(pixel)
-		try:
+	parentIterator = iter(im.getdata())
+
+	for row in range(w):
+		for col in range(h):
+			pixel = np.array(next(parentIterator))
 			childPixel = np.array(next(childIterator))
 			dist = np.linalg.norm(pixel-childPixel)
 
 			if dist < distanceThreshold:
 				close += 1
+				finalImage[row][col] = childPixel
 			total += 1
-			if dist < distanceThreshold:
-				resultantImageFlat.append(list(pixel))
-			else:
-				resultantImageFlat.append([255, 255, 255])
-		except (StopIteration, ValueError) as error:
-			print(pixel)
-			print(childPixel)
-			print(file)
-			break
+
 
 print("close: " + str(close) + ", total: " + str(total) + " percent: " + str(float(close)/float(total) * 100))
-
-finalImage = np.zeros((h, w, 3), dtype=np.uint8)
-
-flatCounter = 0
-for row in range(im.size[1]):
-	currRow = []
-	for col in range(im.size[0]):
-		finalImage[row, col] = resultantImageFlat[flatCounter]
-		flatCounter += 1
-
-# print(finalImage)
 
 img = Image.fromarray(finalImage, 'RGB')
 img.show()
