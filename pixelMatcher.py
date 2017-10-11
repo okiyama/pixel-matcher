b96cf61cb5d11c70d21d11071c8d953e072a62ec
Julian Jocque <lamabuny@gmail.com>
2017-10-10 21:06:05 -0400

    split into functions

--- pixelMatcher.py
+++ pixelMatcher.py
@@ -4,35 +4,45 @@ from os.path import isfile, join
 import numpy as np
 import sys
 
-childFolder = "./images"
-childFiles = [f for f in listdir(childFolder) if isfile(join(childFolder, f))]
-im = Image.open("./BoYT0qQ8.jpg")
-w = im.size[0]
-h = im.size[1]
+def main():
+	childFolder = "./images"
+	im = Image.open("./BoYT0qQ8.jpg")
+	distanceThreshold = int(sys.argv[1]) if len(sys.argv) > 1 else 75
 
-finalImage = np.zeros((w, h, 3), dtype=np.uint8)
-
-distanceThreshold = int(sys.argv[1]) if len(sys.argv) > 1 else 75
-close = 0
-total = 0
-for file in childFiles:
-	childImage = Image.open(join(childFolder, file))
-	childIterator = iter(childImage.getdata())
-	parentIterator = iter(im.getdata())
-
-	for row in range(w):
-		for col in range(h):
-			pixel = np.array(next(parentIterator))
-			childPixel = np.array(next(childIterator))
-			dist = np.linalg.norm(pixel-childPixel)
-
-			if dist < distanceThreshold:
-				close += 1
-				finalImage[row][col] = childPixel
-			total += 1
+	imageCompare(distanceThreshold, im, childFolder, "./output/out.bmp")
 
 
-print("close: " + str(close) + ", total: " + str(total) + " percent: " + str(float(close)/float(total) * 100))
+def imageCompare(distanceThreshold, parentImage, childFolder, outputFileName = None):
+	childFiles = [f for f in listdir(childFolder) if isfile(join(childFolder, f))]
+	w = parentImage.size[0]
+	h = parentImage.size[1]
 
-img = Image.fromarray(finalImage, 'RGB')
-img.show()
+	finalImage = np.zeros((w, h, 3), dtype=np.uint8)
+
+	close = 0
+	total = 0
+	for file in childFiles:
+		childImage = Image.open(join(childFolder, file))
+		childIterator = iter(childImage.getdata())
+		parentIterator = iter(parentImage.getdata())
+
+		for row in range(w):
+			for col in range(h):
+				pixel = np.array(next(parentIterator))
+				childPixel = np.array(next(childIterator))
+				dist = np.linalg.norm(pixel-childPixel)
+
+				if dist < distanceThreshold:
+					close += 1
+					finalImage[row][col] = childPixel
+				total += 1
+
+
+	print("close: " + str(close) + ", total: " + str(total) + " percent: " + str(float(close)/float(total) * 100))
+
+	img = Image.fromarray(finalImage, 'RGB')
+	if outputFileName is not None:
+		img.save(outputFileName)
+
+if __name__ == "__main__":
+	main()
