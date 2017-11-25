82421e72fe7ded076847984f668efbf71e344835
Julian Jocque <lamabuny@gmail.com>
2017-11-25 14:26:44 -0500

    Refactor to use class and functions. Pre-compute distances for huge performance boost

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ TODO:
  * Rather than all or nothing on each pixel, have the pixel show the relative frequency through it's alpha value.  
  * Average the pixel values or choose median pixel  
  * Let closer pixel trump further  
- 
+ * Generate gifs of the output with different thresholds
+ * Automatically resize child images to match parent
--- pixelMatcher.py
+++ pixelMatcher.py
@@ -1,58 +1,159 @@
 from PIL import Image
 from os import listdir
-from os.path import isfile, join
+from os.path import join
+import os
 import numpy as np
 import sys
+import cProfile
+import math
+import subprocess
+import time
+import shutil
 
-def main():
-	childFolder = "./images"
-	outputFolder = "./girl/"
-	im = Image.open("./ff26454c6a2a626a12085b3b6c61e96d--dry-skin-your-skin.jpg")
+#TODO:  Pass in parentImage to compare functions
+#		Make gif automatically
+#		
 
-	if len(sys.argv) > 1:
-		imageCompare(int(sys.argv[1]), im, childFolder, outputFolder + "outCustom" + sys.argv[1] + ".png", False, True)
-	else:
-		for i in range(1,300):
-			imageCompare(i, im, childFolder, outputFolder + "out" + str(i) + ".png", True)
+class PixelMatcher:
+	#if parentFolder is defined, then we loop through the given folder rather than just doing the one parentImage
+	def __init__(self, childFolder, parentImagePath, customDiffThreshold=None):
+		self.parentImage = Image.open(parentImagePath)
+		self.childImages = [Image.open(join(childFolder, f)) for f in listdir(childFolder) if os.path.isfile(join(childFolder, f))]
+		self.customDiffThreshold = customDiffThreshold
+		self.imageWidth = self.parentImage.size[0]
+		self.imageHeight = self.parentImage.size[1]
+		self.diffMap = self.initDiffMap()
+
+		# #TODO, this should just be a function call to writeComparedImage
+		# #Probably should always pass in parent folder honestly
+		# if customDiffThreshold is not None:
+		# 	self.imageCompare(im, childFolder, outputFolder + "outCustom" + sys.argv[1] + ".png", True)
+		# else:
+		# 	folderCount = 0
+		# 	#TODO move this logic into an init method so self.parentFiles is actual Image objects
+		# 	for file in self.parentImages:
+		# 		folderCount += 1
+		# 		parentFilePath = join(parentFolder, file)
+		# 		im = Image.open(parentFilePath)
+		# 		outputFolderPath = join(outputFolder, str(folderCount)) 
+
+		# 		if not os.path.exists(outputFolderPath):
+	 #  				os.makedirs(outputFolderPath)
+
+		# 		for i in range(1, loops, stepSize):
+		# 			self.imageCompare(i, im, childFolder, outputFolderPath + "/out" + format(i, '05') + ".png", True)
+
+	def initDiffMap(self):
+		diffMap = {}
+		for childImage in self.childImages:
+			diffMap[childImage.filename] = np.zeros((self.imageWidth, self.imageHeight, 1), dtype=np.uint8)
+			childIterator = iter(childImage.getdata())
+			parentIterator = iter(self.parentImage.getdata())
+
+			for row in range(self.imageWidth):
+				for col in range(self.imageHeight):
+					pixel = next(parentIterator)
+					childPixel = next(childIterator)
+					diffMap[childImage.filename][row][col] = self.distance(pixel, childPixel)
+
+		return diffMap
+
+	def clearOutputFolder(self, outputFolder):
+		text = input("Clear contents of folder " + outputFolder + " ? ")
+		if(text.lower() == "y"):
+			shutil.rmtree(outputFolder)
+			os.makedirs(outputFolder)
 
 
-def imageCompare(distanceThreshold, parentImage, childFolder, outputFileName = None, maxDistance = False, minDistance = False):
-	childFiles = [f for f in listdir(childFolder) if isfile(join(childFolder, f))]
-	w = parentImage.size[0]
-	h = parentImage.size[1]
+	def makeCompareGif(self, outputFolder, maxMin="max", loops=300, stepSize=1, gifOutputFolder="./gifs"):
+		self.clearOutputFolder(outputFolder)
 
-	finalImage = np.zeros((w, h, 3), dtype=np.uint8)
-	maxDistances = np.zeros((w, h, 1), dtype=np.uint8)
-	minDistances = np.full((w, h, 1), 999999999, dtype=np.uint8)
+		for i in range(1, loops, stepSize):
+			outputFileName = outputFolder + "/" + maxMin + "out" + format(i, '05') + ".png"
+			if maxMin == "max":
+				self.maxCompareImage(i, outputFileName)
+			elif maxMin == "min":
+				self.minCompareImage(i, outputFileName)
+			else:
+				raise ValueError()
 
-	close = 0
-	total = 0
-	for file in childFiles:
-		childImage = Image.open(join(childFolder, file))
-		childIterator = iter(childImage.getdata())
-		parentIterator = iter(parentImage.getdata())
+		self.makeGif(outputFolder, gifOutputFolder)
 
-		for row in range(w):
-			for col in range(h):
-				pixel = np.array(next(parentIterator))
-				childPixel = np.array(next(childIterator))
-				dist = np.linalg.norm(pixel-childPixel)
+	def makeGif(self, pngFolder, gifFolder):
+		gifOutputFile = gifFolder + "/animation" + str(int(time.time())) + ".gif"
+		open(gifOutputFile, "w")
+		os.path.dirname(os.path.realpath(__file__))
+		subprocess.call("convert -delay 0 -loop 0 " + pngFolder + "*.png " + gifOutputFile, shell=True)
 
-				if dist < distanceThreshold or (maxDistance and dist > maxDistances[row][col]) or (minDistance and dist < minDistances[row][col]):
-					if(maxDistance):
+	def maxCompareImage(self, distanceThreshold, outputFileName):
+		finalImage = np.zeros((self.imageWidth, self.imageHeight, 3), dtype=np.uint8)
+		maxDistances = np.zeros((self.imageWidth, self.imageHeight, 1), dtype=np.uint8)
+
+		close = 0
+		total = 0
+		for childImage in self.childImages:
+			childIterator = iter(childImage.getdata())
+			parentIterator = iter(self.parentImage.getdata())
+
+			for row in range(self.imageWidth):
+				for col in range(self.imageHeight):
+					childPixel = next(childIterator)
+					dist = self.diffMap[childImage.filename][row][col]
+
+					if dist < distanceThreshold and dist > maxDistances[row][col]:
 						maxDistances[row][col] = dist
-					if minDistance:
+						close += 1
+						finalImage[row][col] = childPixel
+					total += 1
+
+		print("close: " + str(close) + ", total: " + str(total) + " percent: " + str(float(close)/float(total) * 100))
+
+		Image.fromarray(finalImage, 'RGB').save(outputFileName)
+		
+
+	def minCompareImage(self, distanceThreshold, outputFileName):
+		finalImage = np.zeros((self.imageWidth, self.imageHeight, 3), dtype=np.uint8)
+		minDistances = np.full((self.imageWidth, self.imageHeight, 1), 999999999, dtype=np.uint8)
+
+		close = 0
+		total = 0
+		for childImage in self.childImages:
+			childIterator = iter(childImage.getdata())
+			parentIterator = iter(self.parentImage.getdata())
+
+			for row in range(self.imageWidth):
+				for col in range(self.imageHeight):
+					childPixel = next(childIterator)
+					dist = self.diffMap[childImage.filename][row][col]
+
+					if dist < distanceThreshold and dist < minDistances[row][col]:
 						minDistances[row][col] = dist
-					close += 1
-					finalImage[row][col] = childPixel
-				total += 1
+						close += 1
+						finalImage[row][col] = childPixel
+					total += 1
+
+		print("close: " + str(close) + ", total: " + str(total) + " percent: " + str(float(close)/float(total) * 100))
+
+		Image.fromarray(finalImage, 'RGB').save(outputFileName)
+
+	def distance(self, t1, t2):
+		return math.sqrt( ((t1[0] - t2[0])**2) + ((t1[1] - t2[1])**2) + ((t1[2] - t2[2])**2) )
 
 
-	print("close: " + str(close) + ", total: " + str(total) + " percent: " + str(float(close)/float(total) * 100))
+#argv parsing here
+def main():
+	customDiffThreshold = None
+	if len(sys.argv) > 1:
+		customDiffThreshold = int(sys.argv[1])
 
-	img = Image.fromarray(finalImage, 'RGB')
-	if outputFileName is not None:
-		img.save(outputFileName)
+	# parentFolder = "./parents/"
+	childFolder = "./abstract/"
+	outputFolder = "./output/"
+	parentImagePath = "./parents/abstract-colorsdd5a-turquoise-sq.jpg"
+	matcher = PixelMatcher(childFolder, parentImagePath, customDiffThreshold=customDiffThreshold)
+
+	# matcher.maxCompareImage(150, outputFolder + "outTest.png")
+	matcher.makeCompareGif(outputFolder, loops=10, maxMin="min")
 
 if __name__ == "__main__":
 	main()
